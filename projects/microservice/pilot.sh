012408fd1da9ebf45cc80e1b6a6b6065ccbf30a3
kamalan <chiajuichou@nycu.edu.tw>
2023-08-17 00:10:59 +0800

    Add the check of K1 state vector in trainer/trainer/dataloader.py

--- projects/microservice/pilot.sh
+++ projects/microservice/pilot.sh
@@ -3,4 +3,7 @@
 # Run exporter in a tmux session
 tmux new-session -s pilot-test -d "CUDA_VISIBLE_DEVICES=0 PROJECT=exporter pinto -p exporter run -e ../.env flask --app=exporter run"
 
-tmux split-window -v "CUDA_VISIBLE_DEVICES=0 PROJECT= trainer PROJECT_DIR=$HOME/deepclean/$PROJECT DATA_DIR=/data/KAGRA_O4a_data pinto -p trainer run -e ../.env train --typeo pyproject.toml script=train architecture=autoencoder"
+# tmux attach -t pilot-test
+# C-b %
+
+CUDA_VISIBLE_DEVICES=0 PROJECT=trainer && PROJECT_DIR=$HOME/deepclean/$PROJECT DATA_DIR=${HOME}/kagra-o4-data IFO=K1 pinto -p trainer run -e ../.env train --typeo pyproject.toml script=train architecture=autoencoder
