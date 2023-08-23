33d8e23741d46562c3e6e763e3507821c1a85931
kamalan <chiajuichou@nycu.edu.tw>
2023-08-23 23:58:12 +0800

    Fixing PROJECT_DIR in .env

--- projects/microservice/pilot.sh
+++ projects/microservice/pilot.sh
@@ -6,4 +6,11 @@ tmux new-session -s pilot-test -d "CUDA_VISIBLE_DEVICES=0 PROJECT=exporter pinto
 # tmux attach -t pilot-test
 # C-b %
 
-CUDA_VISIBLE_DEVICES=0 PROJECT=trainer && PROJECT_DIR=$HOME/deepclean/$PROJECT DATA_DIR=${HOME}/kagra-o4-data IFO=K1 pinto -p trainer run -e ../.env train --typeo pyproject.toml script=train architecture=autoencoder
+CUDA_VISIBLE_DEVICES=0 PROJECT=trainer && PROJECT_DIR=$HOME/deepclean/$PROJECT DATA_DIR=/data/ll_data IFO=K1 pinto -p trainer run -e ../.env train --typeo pyproject.toml script=train architecture=autoencoder
+
+CUDA_VISIBLE_DEVICES=1 singularity exec \
+    --nv /home/kamalan/triton/tritonserver.sif \
+    /opt/tritonserver/bin/tritonserver \
+        --model-repository /home/deepclean/results/ \
+        --model-control-mode poll \
+        --repository-poll-secs 10 \
